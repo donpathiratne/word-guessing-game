237f2459bbf7c28270b56a0884a60402c65a8c41
Yehan Pathiratne <yehanpathirathne@gmail.com>
2025-12-24 19:28:46 +0530

    Update word-guessing.py

--- word-guessing.py
+++ word-guessing.py
@@ -9,5 +9,33 @@ print('YOU HAVE BEEN GIVEN SOME WORDS.');print()
 words= ['ambiguous','conscientious','inevitable','meticulous','notorious',
         'obsolete','paradox','resilient','superficial','vulnerable']
 
-print(words);print()
-print('GUESS THE WORD...GOOD LUCK!!!')
+print(words)
+print('GUESS THE WORD WITHIN LIMITED TURNS...\nGOOD LUCK!!!');print()
+print('Meaning:',end=' ')
+secret_word= random.choice(words)
+if secret_word=='ambiguous':
+    print("When something is not clear and people understand it in different ways.")
+elif secret_word=='conscientious':
+    print("A person who does their work properly and cares about doing it right. ")
+elif secret_word=='inevitable':
+    print("Something that will happen no matter what.")
+elif secret_word=='meticulous':
+    print("Someone who is extremely careful, even with small things.")
+elif secret_word=='notorious':
+    print("Famous for bad things, not good ones.")
+elif secret_word=='obsolete':
+    print("Too old to use now because better thigs exist.")
+elif secret_word=='paradox':
+    print("Something that sounds wrong, but is actually true.")
+elif secret_word=='resilient':
+    print("Able to be strong again after problems.")
+elif secret_word=='superficial':
+    print("Only looks at the outside, not the real meaning.")
+else:
+    print("Easy to hurt or get harmed.")
+print()
+
+length_secret_word= len(secret_word) # get the length of secret word
+turns= length_secret_word +1         # only give one additional turn to guess the word
+
+# now we have to make the part that get letters and match them with secret word
