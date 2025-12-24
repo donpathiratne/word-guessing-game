8c8187bcc59de2857441cc999c6a7907a55f403c
Yehan Pathiratne <yehanpathirathne@gmail.com>
2025-12-24 17:26:36 +0530

    Create word-guessing.py

---
+++ word-guessing.py
@@ -0,0 +1,10 @@
+'''*** Word guessing Game ***'''
+
+import random
+
+user_name= input('Enter Your Name: ')
+print(f'HELLO {user_name}\nWELCOME TO THE WORD GUESSING GAME...');print()
+print('YOU HAVE BEEN GIVEN SOME WORDS.')
+
+words= ['ambiguous','conscientious','inevitable','meticulous','notorious',
+        'obsolete','paradox','resilient','superficial','vulnerable']
